57f79b455c0e9842507e23e25efd8ede2313dc44
AlirezaRobix <AlirezaRobix@gmail.com>
2020-09-07 20:07:51 +0430

    Update filters

--- tabchi.py
+++ tabchi.py
@@ -1,4 +1,4 @@
-from pyrogram import Filters, Client
+from pyrogram import filters, Client
 import pyrogram
 
 appid = 1311366
@@ -6,12 +6,12 @@ apphash = "55413c0f83d2d0553d059bc33b47ca71"
 
 app = Client("test", api_id=appid, api_hash=apphash)
 
-@app.on_message(Filters.group & Filters.command("ping", prefixes=""))
+@app.on_message(filters.group & filters.command("ping", prefixes=""))
 def ping(c,m):
     m.reply_text("**Pong**")
 
 botlists = []
-@app.on_message(Filters.group & Filters.command("add", prefixes=""))
+@app.on_message(filters.group & filters.command("add", prefixes=""))
 def adder(c,m):
     try:
         botlists.append(m.command[1])
@@ -24,21 +24,21 @@ def adder(c,m):
     except:
         app.send_message(m.chat.id, "**Error!**")
 
-@app.on_message(Filters.group & Filters.command("invite",prefixes=""))
+@app.on_message(filters.group & filters.command("invite",prefixes=""))
 def invite(c,m):
     s = app.send_message(m.chat.id, "**Start Adding Bots...**")
     print(f"\033[1;32;40mStart Adding Bots To Group: \033[1;33;40m|{m.chat.title}|")
     app.add_chat_members(m.chat.id, botlists)
     app.delete_messages(s.chat.id, s.message_id)
 
-@app.on_message(Filters.group & Filters.command("list",prefixes=""))
+@app.on_message(filters.group & filters.command("list",prefixes=""))
 def lists(c,m):
     if botlists == []:
         app.send_message(m.chat.id, "**List Is Empty!**")
     else:
         app.send_message(m.chat.id, "**Bots:\n"+"\n".join(botlists)+"**")
 
-@app.on_message((Filters.group | Filters.private) & Filters.command("clear",prefixes=""))
+@app.on_message((filters.group | filters.private) & filters.command("clear",prefixes=""))
 def clears(c,m):
     botlists.clear()
     app.send_message(m.chat.id, "**Bot List Successfuly Cleared!**")
